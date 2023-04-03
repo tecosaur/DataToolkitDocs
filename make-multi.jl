3123d08c95b630a3c0a715f5ff306c1a485bb630
TEC <git@tecosaur.net>
2023-04-04 02:32:49 +0800

    tweak the multidoc style

--- make-multi.jl
+++ make-multi.jl
@@ -29,6 +29,48 @@ MultiDocumenter.make(
         engine = MultiDocumenter.FlexSearch),
     rootpath = "DataToolkitDocs")
 
+# --- Edits ---
+
+let multidoc_style_overrides = [
+    """
+#multi-page-nav {
+    width: 100%;
+    height: var(--navbar-height);
+    z-index: 10;
+    padding: 0 1rem;
+    position: sticky;
+    display: flex;
+    top: 0;
+    background-color: #282f2f;
+    border-bottom: 1px solid #5e6d6f;
+}""" => """
+#multi-page-nav {
+    width: 100%;
+    height: var(--navbar-height);
+    z-index: 10;
+    padding: 0 1rem;
+    position: sticky;
+    display: flex;
+    top: 0;
+    background-color: whitesmoke;
+    border-bottom: 1px solid #dbdbdb;
+}
+
+html.theme--documenter-dark #multi-page-nav {
+    background-color: #282f2f;
+    border-bottom: 1px solid #5e6d6f;
+}""",
+    "font-size: 14px;" => "font-size: 1.1em;",
+    "\n    text-transform: uppercase;" => "",
+    "color: #fff;" => "color: inherit;\n    opacity: 1;",
+    "color: #ccc;" => "color: inherit;\n    opacity: 0.75;"
+    ]
+    multidoc_css_file = joinpath(outpath, "assets", "default", "multidoc.css")
+    chmod(multidoc_css_file, 0o664)
+    multidoc_style = read(multidoc_css_file, String)
+    write(multidoc_css_file, replace(multidoc_style, multidoc_style_overrides...))
+end
+
 # --- Push to gh-pages ---
 
 outbranch = "gh-pages"
